5707ce7af78adb15913591dc597ed6bedb470631
HunterSD23 <148793726+HunterSD23@users.noreply.github.com>
2024-09-30 14:22:24 +0000

    experimented with while loops

---
+++ whileLoops.py
@@ -0,0 +1,32 @@
+"""
+Author: Hunter Dettmer
+Program: whileLoops.py
+"""
+
+theSum = 0.0
+data = input("Enter a number or just enter to quit: ")
+
+while data != "":
+    number = float(data)
+    theSum += number
+    data = input("Enter a number or just enter to quit: ")
+
+print("The sum of your numbers is:", theSum)
+
+print("\n\n\nComparing forLoop to while Loop")
+print("----------------------------------\n")
+
+# Summation with for Loop
+theSum = 0
+for count in range(1, 100001):
+    theSum += count
+print(theSum)
+
+# Summation with while Loops
+theSum = 0 
+count = 1
+while count <= 100000:
+    theSum += count
+    count += 1
+
+print("Our while Loop version: ", theSum)
