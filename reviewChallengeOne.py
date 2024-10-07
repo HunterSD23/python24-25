d2a70373f7bd158eada8d39686ecf731d0575fd2
HunterSD23 <148793726+HunterSD23@users.noreply.github.com>
2024-10-07 13:27:55 +0000

    first challenge completed

--- reviewChallengeOne.py
+++ reviewChallengeOne.py
@@ -2,3 +2,23 @@
 Program: reviewChallengeOne.py
 Author: Hunter Dettmer
 """
+
+numberone = int(input("Please enter a number: "))
+operation = input("Please enter the operation you would like to use in word form: ")
+numbertwo = int(input("Please enter another number: "))
+
+if operation == "plus":
+    answer = numberone + numbertwo
+elif operation == "minus":
+    answer = numberone - numbertwo
+elif operation == "times":
+    answer = numberone * numbertwo
+elif operation == "divide":
+    answer = numberone / numbertwo
+else:
+    answer = print("You have given an invalid operation")
+print("\n")
+if answer == print("You have given an invalid operation"):
+    print("Please try entering an operation like 'plus' or 'minus'")
+else:    
+    print("The solution to", numberone, operation, numbertwo, "is...", answer)
